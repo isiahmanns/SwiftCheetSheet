e9daa9a5631e68c5ff42cbd87d4224daa07484ab
Isiah Manns <isiah.michael1018@gmail.com>
2022-12-07 17:37:58 -0500

    Brain dump of dictionary notes

--- SwiftCheatSheet.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
+++ SwiftCheatSheet.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
@@ -1,7 +1,38 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// TODO: - Clean this up
 
-//: [Next](@next)
+typealias SnackID = String
+
+struct Snack {
+    let id: String = UUID().uuidString
+    let name: String
+    let price: Int
+}
+
+let popcorn = Snack(name: "Popcorn", price: 4)
+let skittles = Snack(name: "Skittles", price: 3)
+let hotcheetos = Snack(name: "Hot Cheetos", price: 3)
+
+var inventory: [SnackID: Snack] = [:]
+
+[popcorn, skittles, hotcheetos].forEach { snack in
+    inventory[snack.id] = snack
+}
+
+let id = hotcheetos.id
+
+// check for key membership
+if let snack = inventory[id] {
+    print(snack)
+}
+guard let snack = inventory[id] else { fatalError() }
+// at this point, item is populated
+
+
+// check for key non-membership
+if inventory[id] == nil {
+    print("item does not exist")
+}
+guard inventory[id] == nil else { fatalError() }
+// at this point, we know item doesn't exist
