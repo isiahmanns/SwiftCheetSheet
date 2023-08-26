faa51efdb85bbdb5561066a73dc14f3051c53794
Isiah Manns <isiah.michael1018@gmail.com>
2023-08-25 21:02:42 -0400

    Array binary search

--- SwiftCheatSheet.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
+++ SwiftCheatSheet.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -4,4 +4,44 @@ import Foundation
 
 var greeting = "Hello, playground"
 
+let data = [
+    "a",
+    "b",
+    //"c",
+    "d",
+    "e",
+    "f",
+    "g"
+]
+
+
+extension Array where Element: Comparable {
+    func binarySearch(for element: Element) -> Bool {
+        if count == 0 {
+            return false
+        }
+
+        let middleIdx = count / 2
+
+        if element == self[middleIdx] {
+            return true
+        } else if element < self[middleIdx] {
+            return Array(self[..<middleIdx]).binarySearch(for: element)
+        } else {
+            return Array(self[(middleIdx + 1)...]).binarySearch(for: element)
+        }
+    }
+}
+
+data[data.count...]
+data[data.count..<data.count]
+let sortedData = data.sorted()
+sortedData.binarySearch(for: "c")
+sortedData.binarySearch(for: "i")
+sortedData.binarySearch(for: "b")
+sortedData.binarySearch(for: "d")
+sortedData.binarySearch(for: "e")
+sortedData.binarySearch(for: "f")
+sortedData.binarySearch(for: "g")
+
 //: [Next](@next)
